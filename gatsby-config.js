5440848a333c8296705cdfbd2728637923e90d1a
iambia <bia.paulaa@gmail.com>
2019-12-09 21:13:27 +0100

    added contentful and access token to dotenv

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,9 @@
+const dotenv = require('dotenv')
+
+if(process.env.NODE_ENV !== 'production') {
+  dotenv.config()
+}
+
 module.exports = {
   siteMetadata: {
     title: `Bia.dev Blog`,
@@ -29,5 +35,12 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceID: 'i0m4cyt7hwly',
+        accessToken: process.env.CONTENTFUL_ACSESS_TOKEN
+      }
+    }
   ],
 }
