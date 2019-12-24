ef55cafc88d384a63093dcd0d2de8b1b7acf4261
iambia <bia.paulaa@gmail.com>
2019-12-24 14:49:00 +0100

    logo fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -68,5 +68,20 @@ module.exports = {
       ],
     },
   },
+  {
+    resolve: "gatsby-plugin-google-tagmanager",
+    options: {
+      id: " GTM-P29D3XB",
+
+      // Include GTM in development.
+      // Defaults to false meaning GTM will only be loaded in production.
+      includeInDevelopment: false,
+
+      // datalayer to be set before GTM is loaded
+      // should be an object or a function that is executed in the browser
+      // Defaults to null
+      defaultDataLayer: { platform: "gatsby" },
+    },
+  },
   ],
 }
