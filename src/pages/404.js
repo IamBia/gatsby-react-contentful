8d715e7a84d520381c3ff58349389ee1e4d78ea7
iambia <bia.paulaa@gmail.com>
2020-01-03 20:07:22 -0300

    warning fix

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,6 @@
 import React from "react"
 
 import Layout from "../components/layout"
-import Header from "../components/header"
 import SEO from "../components/seo"
 import banana from "../images/giphy.gif"
 
