7eda8ca24c307ead07148c4f0aa77a846c71a455
iambia <bia.paulaa@gmail.com>
2020-01-03 20:30:13 -0300

    page fix

--- src/components/page.js
+++ src/components/page.js
@@ -1,10 +1,10 @@
-import React from 'react'
-import Components from './components.js';
+// import React from 'react'
+// import Components from './components.js';
 
-const Page = (props) => (
-  <div>
-    {props.blok.body && props.blok.body.map((blok) => React.createElement(Components(blok.component), {key: blok._uid, blok: blok}))}
-  </div>
-)
+// const Page = (props) => (
+//   <div>
+//     {props.blok.body && props.blok.body.map((blok) => React.createElement(Components(blok.component), {key: blok._uid, blok: blok}))}
+//   </div>
+// )
 
-export default Page
+// export default Page
